1166edb4a4316dc263a2d730c65c859805ed5881
Md Ishaq <iamishaque.business@gmail.com>
2022-09-25 23:37:37 +0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,49 +1,123 @@
-import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Stack
+} from "@mui/material";
 import axios from "axios";
-import { useCSVReader } from 'react-papaparse';
+import { useEffect, useState } from "react";
+import { useCSVReader } from "react-papaparse";
 import { useDispatch } from "react-redux";
-import './App.css';
-import EnhancedTable from "./components/Table";
-import { setProductList } from "./redux/state/products.slice";
-const styles = {
-  csvReader: {
-    display: 'flex',
-    flexDirection: 'row',
-    marginBottom: 10,
-  },
-  browseFile: {
-    width: '20%',
-  },
-  acceptedFile: {
-    border: '1px solid #ccc',
-    height: 45,
-    lineHeight: 2.5,
-    paddingLeft: 10,
-    width: '80%',
-  },
-  remove: {
-    borderRadius: 0,
-    padding: '0 20px',
-  },
-  progressBarBackgroundColor: {
-    backgroundColor: 'red',
-  },
-};
+import "./App.css";
+const styles = {};
+
+const chunkSize = 10 * 1024;
 function App() {
   const dispatch = useDispatch();
   const { CSVReader } = useCSVReader();
-  let url = "http://localhost:5000/api/v1/product"
+  let url = "http://localhost:5000/api/v1/product/upload?";
   function onUploadHandler(results) {
     // dispatch(setProductList(results))
     console.log("results", results);
-    axios.post(url, { data: results })
+    axios
+      .post(url, { data: results })
       .then(({ data }) => console.log("Data results", data))
-      .catch(err => console.log("err", err))
+      .catch((err) => console.log("err", err));
   }
+  const [dropZoneActive, setDropZoneActive] = useState(false);
+  const [files, setFiles] = useState([]);
+  const [currentFileIndex, setCurrentFileIndex] = useState(null);
+  const [lastUploadedFileIndex, setLastUploadedFileIndex] = useState(null);
+  const [currentChunkIndex, setCurrentChunkIndex] = useState(null);
+
+  function handleDrop(e) {
+    e.preventDefault();
+    setFiles((s) => [...s, ...e.dataTransfer.files]);
+  }
+
+  function readAndUploadCurrentChunk() {
+    const reader = new FileReader();
+    const file = files[currentFileIndex];
+    if (!file) {
+      return;
+    }
+    const from = currentChunkIndex * chunkSize;
+    const to = from + chunkSize;
+    const blob = file.slice(from, to);
+    reader.onload = (e) => uploadChunk(e);
+    reader.readAsDataURL(blob);
+  }
+
+  function uploadChunk(readerEvent) {
+    const file = files[currentFileIndex];
+    const data = readerEvent.target.result;
+    const params = new URLSearchParams();
+    params.set("name", file.name);
+    params.set("size", file.size);
+    params.set("currentChunkIndex", currentChunkIndex);
+    params.set("totalChunks", Math.ceil(file.size / chunkSize)); //250MB
+
+    // const headers = { "Content-Type": "application/json" };
+    const headers = { "Content-Type": "application/octet-stream" };
+    axios
+      .post(
+        "http://localhost:5001/upload?" + params.toString(),
+        data,
+        { headers }
+      )
+      .then(({ data }) => {
+        const file = files[currentFileIndex];
+        const fileSize = files[currentFileIndex].size;
+        const isLastChunk = currentChunkIndex === Math.ceil(fileSize / chunkSize) - 1
+        if(isLastChunk){
+          file.finalFileName = data.result
+          setLastUploadedFileIndex(currentFileIndex)
+          setCurrentChunkIndex(null)
+        }else{
+          setCurrentChunkIndex(s => s + 1)
+        }
+      })
+      .catch((err) => console.log("err", err));
+  }
+
+  useEffect(()=>{
+    if(lastUploadedFileIndex === null){
+      return;
+    }
+    const isLastFile = lastUploadedFileIndex === files.length - 1;
+    const nextFileIndex = isLastFile ? null : currentFileIndex + 1;
+    setCurrentFileIndex(nextFileIndex)
+  },[lastUploadedFileIndex])
+  useEffect(() => {
+    if (files.length > 0) {
+      if (currentFileIndex === null) {
+        setCurrentFileIndex(
+          lastUploadedFileIndex === null ? 0 : lastUploadedFileIndex + 1
+        );
+      }
+    }
+  }, [files.length]);
+
+  useEffect(() => {
+    if (currentFileIndex !== null) {
+      setCurrentChunkIndex(0);
+    }
+  }, [currentFileIndex]);
+
+  useEffect(() => {
+    if (currentChunkIndex !== null) {
+      readAndUploadCurrentChunk();
+    }
+  }, [currentChunkIndex]);
+
   return (
-    <Stack sx={{ marginTop: "1em" }} className="App">
-      <Box>
-        <Button variant='contained' htmlFor="file_input_field" sx={{ width: "10em", mx: "auto" }} component="label">
+    <Box sx={{ pt: "1em" }}>
+      <Stack>
+        <Button
+          variant="contained"
+          htmlFor="file_input_field"
+          sx={{ width: "10em", mx: "auto" }}
+          component="label"
+        >
           Upload CSV
         </Button>
         <input
@@ -52,11 +126,25 @@ function App() {
           hidden
           onChange={(e) => onUploadHandler(e.target.files)}
         />
-        {/* <Typography>{acceptedFile && acceptedFile.name}</Typography>
-        <Button variant='contained' sx={{ width: "10em", mx: "auto" }} >Remove</Button> */}
-      </Box>
+      </Stack>
+      <div
+        className={`dropZone ${dropZoneActive ? "active" : ""}`}
+        onDragOver={(e) => {
+          setDropZoneActive(true);
+          e.preventDefault();
+        }}
+        onDragLeave={(e) => {
+          setDropZoneActive(false);
+          e.preventDefault();
+        }}
+        onDrop={(e) => {
+          handleDrop(e);
+        }}
+      >
+        Drop your files here
+      </div>
       {/* <EnhancedTable /> */}
-    </Stack>
+    </Box>
   );
 }
 
